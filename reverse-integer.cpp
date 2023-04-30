4317653ce1de650ec1bbc82e08f241db725731b6
IshuPandey <ishupandey39@gmail.com>
2023-04-30 13:38:51 +0530

    reverse-integer

---
+++ reverse-integer.cpp
@@ -0,0 +1,19 @@
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+class Solution {
+public:
+    int reverse(int x) {
+       long int y=0;
+        while(x!=0)
+        {
+                int rem=x%10;   
+                y=y*10+rem;
+                x=x/10;
+        }
+        if(y<pow(-2,31)|| y>pow(2,31)+1)
+            return 0;
+        return y;    
+        
+    }
+};
